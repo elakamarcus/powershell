b5f3a0278c22e7143656bd78a00e250b89b96344
elakamarcus <n0m4d@hotmail.com>
2016-07-18 14:21:10 +0800

    Adjust formulation of error message

--- ps_machineonofftimes.ps1
+++ ps_machineonofftimes.ps1
@@ -26,15 +26,14 @@ function Get-SystemPoweredOnOff {
 function getSecLogs ($PCName) {
   Get-EventLog -LogName System -ComputerName $PCName | ?{ $_.EventID -eq 6006 -or $_.EventID -eq 6005} 
 }
-if (!$args[0]) {
-  Write-Host "[!] Execute with machine name as parameter."  
-  Write-Host "[+] Execute as $args[-1] ComputerName"
-  Write-Host "[+] Optional parameter how many entries as single int, e.g. 10. Default is 5."
+if (!$args[0] -or $args[2]) {
+  Write-Host "[!] Too many or too few parameters."
+  Write-Host "[!] Execute with machine name as parameter."
+  Write-Host "[+] Optional parameter how many entries as a single int, e.g. 10. Default is 5."
 }
 if($args[1]) {
   $Global:list = $args[1]
 }
-
 try {
   getSecLogs $args[0] | select -First $Global:list @{N="Type";E={GetBootTypeFromID $_.EventID}}, @{N="Time";E={$_.TimeGenerated}}    
 }

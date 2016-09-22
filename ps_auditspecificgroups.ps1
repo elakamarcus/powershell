f9b47b40a0b44a1885ab05f88741ec9ef986cd1f
elakamarcus <n0m4d@hotmail.com>
2016-09-22 14:37:24 +0800

    update to more appropriate logic

--- ps_auditspecificgroups.ps1
+++ ps_auditspecificgroups.ps1
@@ -1,6 +1,6 @@
 #Suppress error messages, because a lot of searches will be made in the wrong domain....
 $ErrorActionPreference="SilentlyContinue"
-#if you are in a multidomain-environment:, if not, just add your only domain :D
+#if you are in a multidomain-environment:, if not, just add your only domain.
 $domains = ("domain1", "domain2", "domain3")
 #define which groups to review, this could be configured to be read from a file, passed as parameter args[0]
 $groups = ("name", "of", "privilege", "security", "groups")
@@ -20,13 +20,14 @@ foreach($group in $groups) {
             $user = Get-ADUser -Identity $id -Server $domain -properties *
             #Very ugly solution instead of throw exception
             #if-statement should not be necessary when user is cleared.
-            if ( $user.name -inotlike $null ) {
+            if ($user.name) {
                 #Print so we can append to file
                 $domain.split(".")[0] + ";" + $group + ";" +  $user.samaccountname + ";" + $user.name +";" + $user.description + ";"
                 #Clear variable
                 $user = "" 
             }
             else {
+                # or else what...? -Exactly!
             }
         }
         #Clear sams for next group/domain
